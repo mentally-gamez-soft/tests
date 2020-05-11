3a59675b175a23ac630349ad7f11a33efb90f277
mentally-gamez-soft <mentally.gamez.soft@gmail.com>
2020-05-11 20:34:47 +0200

    Completion of all the calculator

--- calculator.py
+++ calculator.py
@@ -1,6 +1,20 @@
 
-# class test of a calculator
+# class calculator
+# Test project for Travis CI
 class Calculator():
 
-    def multiply(p_val1: int, p_val2: int) -> int:
+    # Multiplication
+    def multiply(self, p_val1: int, p_val2: int) -> int:
         return p_val1 * p_val2
+
+    # substraction
+    def substract(self, p_val1: int, p_val2: int) -> int:
+        return p_val1 - p_val2
+
+    # addition
+    def addition(self, p_val1: int, p_val2: int) -> int:
+        return p_val1 + p_val2
+
+    # addition
+    def divide(self, p_val1: int, p_val2: int) -> int:
+        return p_val1 / p_val2
--- test_calc.py
+++ test_calc.py
@@ -2,7 +2,25 @@
 from calculator import Calculator
 
 
+def test_multiplyADD():
+    x, y = 10, 2
+    instance = Calculator()
+    assert instance.add(x, y) == x * y, "Add method has a problem"
+
+
+def test_substract():
+    x, y = 10, 2
+    instance = Calculator()
+    assert instance.substract(x, y) == x - y, "Substract method has a problem"
+
+
 def test_multiply():
     x, y = 10, 2
     instance = Calculator()
     assert instance.multiply(x, y) == x * y, "Multiply method has a problem"
+
+
+def test_divide():
+    x, y = 10, 2
+    instance = Calculator()
+    assert instance.divide(x, y) == x / y, "Divide method has a problem"
